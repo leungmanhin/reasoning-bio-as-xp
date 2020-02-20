10a530d8185a877e76471ad9752112346d3d4ea3
leungmanhin <manhinleung@gmail.com>
2020-02-20 19:27:10 +0800

    Add a util function for downloading all the gene names

---
+++ name-disambiguation.scm
@@ -0,0 +1,25 @@
+(use-modules (ice-9 rdelim))
+
+(define symbol-filename "")
+(define symbol-alist '())
+
+(define (download-symbols)
+"
+Download all the gene symbols -- Approved symbol, Previous symbols,
+and Alias symbols, from genenames.org.
+"
+    (define base-url "https://www.genenames.org/cgi-bin/download/custom?")
+    (define args
+        (list "col=gd_app_sym&"
+              "col=gd_prev_sym&"
+              "col=gd_aliases&"
+              "status=Approved&"
+              "status=Entry%20Withdrawn&"
+              "hgnc_dbtag=on&"
+              "order_by=gd_app_sym_sort&"
+              "format=text&submit=submit"))
+    (set! symbol-filename
+        (string-append "gene_names_" (strftime "%d-%m-%y" (localtime (current-time))) ".txt"))
+
+    (system (string-append "curl '" base-url (string-join args "") "' --output " symbol-filename))
+)
