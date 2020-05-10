99890d4a789f265603a8128d9c30b8ab3fb71432
leungmanhin <manhinleung@gmail.com>
2020-05-11 01:10:10 +0800

    Get wanted members

--- embedding-analysis.scm
+++ embedding-analysis.scm
@@ -92,12 +92,21 @@
 ;; --- Step 3: Generate SubsetLinks for the above members
 (format #t "--- Directly introducting SubsetLinks for the concepts...\n")
 (define (inh-from-pair p) (Inheritance (gar p) (gdr p)))
-(define (rev-inh-from-pair p) (Inheritance (gdr p) (gar p)))
-(define all-members
-  (append (get-member-links 'GeneNode 'ConceptNode) (get-member-links 'ConceptNode 'ConceptNode)))
-(define inhs (map inh-from-pair all-members))
-(define rev-inhs (map rev-inh-from-pair all-members))
-(define all-inhs (append inhs rev-inhs))
+(define indirect-pairs
+  (cog-outgoing-set
+    (cog-execute!
+      (Bind
+        (VariableSet
+          (TypedVariable (Variable "$X") (TypeChoice (Type "ConceptNode") (Type "GeneNode")))
+          (TypedVariable (Variable "$Y") (Type "ConceptNode"))
+          (TypedVariable (Variable "$Z") (Type "ConceptNode")))
+        (And
+          (Present
+            (Member (Variable "$X") (Variable "$Y"))
+            (Member (Variable "$X") (Variable"$Z")))
+          (Not (Equal (Variable"$Y") (Variable"$Z"))))
+        (List (Variable "$Y") (Variable"$Z"))))))
+(define all-inhs (map inh-from-pair indirect-pairs))
 (pln-add-rule-by-name "subset-direct-introduction-rule")
 (define step-3-results (append (cog-get-atoms 'SubsetLink) (append-map cog-outgoing-set (map pln-bc all-inhs))))
 
